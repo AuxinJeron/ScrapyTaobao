0b64b91b9af21ec241a5bbba97247a0d1f1f8513
Yang Li <panbabybaby@gmail.com>
2017-10-22 18:20:42 -0700

    Add more attributes

--- taobao/items.py
+++ taobao/items.py
@@ -12,7 +12,16 @@ class TaobaoItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     collection = scrapy.Field()
+    nid = scrapy.Field()
+    pid = scrapy.Field()
     title = scrapy.Field()
     price = scrapy.Field()
+    category = scrapy.Field()
     seller = scrapy.Field()
+    seller_id = scrapy.Field()
+    comments = scrapy.Field()
+    sales = scrapy.Field()
+    location = scrapy.Field()
+    pic_url = scrapy.Field()
+    detail_url = scrapy.Field()
     pass
